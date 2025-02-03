f15a47e76e7e107197b034ff9da121d9beae9d14
tom mcandrew <mcandrew@lehigh.edu>
2025-02-02 22:48:08 -0500

    Hospitalization data

---
+++ get_target_data.R
@@ -0,0 +1,48 @@
+#' Fetch FluSight target Data 
+#' Obtain influenza signal
+#'
+#' @return data frame of flu incidence with columns date, location,
+#'   location_name, value, weekly_rate
+#' 
+fetch_flu <- function(){
+  require(dplyr)
+  require(lubridate)
+  require(RSocrata)
+  require(stringr)
+  
+  #read data from data.cdc.gov, filtering for when flu reporting became mandatory
+  health_data = RSocrata::read.socrata(url = "https://data.cdc.gov/resource/mpgq-jmmr.json?$limit=999999") #%>%
+  
+  #remove  VI and AS as they are not included for FluSight, keep only necessary vars and add epiweek and epiyear 
+  recent_data = health_data %>% 
+    dplyr::filter(!jurisdiction %in% c("VI", "AS", "GU", "MP")) %>% 
+    dplyr::select(jurisdiction, weekendingdate, totalconfflunewadm) %>% 
+    dplyr::rename("value" = "totalconfflunewadm", "date"="weekendingdate", "state"="jurisdiction") %>% 
+    dplyr::mutate(date = as.Date(date), 
+                  value = as.numeric(value),
+                  state = str_replace(state, "USA", "US"))
+  
+  #bind state population data
+  full_data = dplyr::left_join(recent_data, locations, by = join_by("state" == "abbreviation"))
+  
+  #calculates weekly rate 
+  final_dat = full_data %>% 
+    dplyr::mutate(weekly_rate = (value*100000)/population ) %>% 
+    select(date, location, location_name, value, weekly_rate)
+  
+  return(final_dat)
+  
+}
+
+library(dplyr)
+library(lubridate)
+library(RSocrata)
+
+locations <- read.csv(file = "https://raw.githubusercontent.com/cdcepi/FluSight-forecast-hub/main/auxiliary-data/locations.csv") %>% dplyr::select(1:4)
+
+target_data <- fetch_flu()
+
+write.csv(target_data, file = "./data_sets/target-hospital-admissions.csv", row.names = FALSE)
+
+
+
